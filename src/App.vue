153a52d2eaca6e05e843e5dfbeeecce5fde53572
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-09-14 10:24:56 +0300

    feat: добавлены eslint и prettier

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,68 @@
 <template>
   <div style="height: 100vh">
-    <yandex-map ref="map" style="width: 100%; height: 100%" :zoom="15" :coords="[59.132296, 37.845130]" :controls="[]">
+    <yandex-map
+      ref="map"
+      style="width: 100%; height: 100%"
+      :zoom="15"
+      :coords="[59.132296, 37.84513]"
+      :controls="[]"
+    >
     </yandex-map>
   </div>
 </template>
 
 <script>
-import { loadYmap } from 'vue-yandex-maps';
-import location from '@/data/location.json';
-import temperatures from '@/data/temperatures.json';
-import locomotive from '@/assets/images/locomotive.svg';
-import mapsmark from '@/assets/images/mapsmark.svg';
+import { loadYmap } from 'vue-yandex-maps'
+import location from '@/data/location.json'
+import temperatures from '@/data/temperatures.json'
+import locomotive from '@/assets/images/locomotive.svg'
+import mapsmark from '@/assets/images/mapsmark.svg'
 
 export default {
-  name: "App",
+  name: 'App',
   data() {
     return {
       points: null,
       locoType: null,
-      locoNumber: null,
-    };
+      locoNumber: null
+    }
   },
 
   created() {
-    const { Latitude: latitudes, Longitude: longitudes,
-      Timestamp: locTimestamp, LocoType: locoType,
-      LocoNumber: locoNumber } = location;
-    const { OutsideTemp: outsideTemps, Timestamp: tempTimestamps } = temperatures;
+    const {
+      Latitude: latitudes,
+      Longitude: longitudes,
+      Timestamp: locTimestamp,
+      LocoType: locoType,
+      LocoNumber: locoNumber
+    } = location
+    const { OutsideTemp: outsideTemps, Timestamp: tempTimestamps } = temperatures
 
-    this.locoType = locoType;
-    this.locoNumber = locoNumber;
+    this.locoType = locoType
+    this.locoNumber = locoNumber
 
     this.points = latitudes.map((latitude, index) => ({
       coordinates: [latitude, longitudes[index]],
-      locationTime: this.getLocationTime(this.getDiffTimestamp(locTimestamp[index],
-        tempTimestamps), outsideTemps)
-    }));
+      locationTime: this.getLocationTime(
+        this.getDiffTimestamp(locTimestamp[index], tempTimestamps),
+        outsideTemps
+      )
+    }))
   },
 
   async mounted() {
     // получаем весь функционал ymaps
-    await loadYmap();
+    await loadYmap()
 
     // извлекаем массив координат всех точек
-    const pointsCoordinates = this.points.map(point => point.coordinates);
+    const pointsCoordinates = this.points.map((point) => point.coordinates)
 
     // cоздаём ломаную линию
-    const polyline = new ymaps.Polyline(
-      pointsCoordinates, null, {
+    const polyline = new ymaps.Polyline(pointsCoordinates, null, {
       draggable: false,
       strokeColor: '#ff0000',
       strokeWidth: 3
-    }
-    );
+    })
 
     // cоздаём описанние локомотива
     const descriptionLoco = new ymaps.control.Button({
@@ -62,65 +72,68 @@ export default {
       },
       options: {
         maxWidth: 250,
-        selectOnClick: false,
+        selectOnClick: false
       }
-    });
+    })
 
     // cоздаём коллекцию меток
-    const placemarkCollection = new ymaps.GeoObjectCollection();
+    const placemarkCollection = new ymaps.GeoObjectCollection()
 
     for (let i = 0; i < this.points.length; i++) {
-      const point = this.points[i];
+      const point = this.points[i]
 
       const placemark = new ymaps.Placemark(
         point.coordinates,
         {
-          hintContent: `температура: ${point.locationTime}℃`,
-        }, {
-        iconLayout: 'default#imageWithContent',
-        iconImageHref: '',
-        iconImageSize: [30, 42],
-        iconImageOffset: [-5, -38]
-      }
-      );
+          hintContent: `температура: ${point.locationTime}℃`
+        },
+        {
+          iconLayout: 'default#imageWithContent',
+          iconImageHref: '',
+          iconImageSize: [30, 42],
+          iconImageOffset: [-5, -38]
+        }
+      )
 
       // добавляем метку в коллекцию
-      placemarkCollection.add(placemark);
+      placemarkCollection.add(placemark)
     }
 
     // добавляем/удаляем метку по ховеру
     placemarkCollection.events
-      .add("mouseenter", function (e) {
-        e.get('target').options.set('iconImageHref', mapsmark);
+      .add('mouseenter', function (e) {
+        e.get('target').options.set('iconImageHref', mapsmark)
       })
-      .add("mouseleave", function (e) {
-        e.get('target').options.set('iconImageHref', '');
-      });
+      .add('mouseleave', function (e) {
+        e.get('target').options.set('iconImageHref', '')
+      })
 
     // добавляем ломаную линию на карту
-    this.$refs.map.myMap?.geoObjects.add(polyline);
+    this.$refs.map.myMap?.geoObjects.add(polyline)
 
     // добавляем описанние локомотива на карту
-    this.$refs.map.myMap?.controls.add(descriptionLoco, { floatIndex: 0 });
+    this.$refs.map.myMap?.controls.add(descriptionLoco, {
+      floatIndex: 0
+    })
 
     // добавляем коллекцию на карту
-    this.$refs.map.myMap?.geoObjects.add(placemarkCollection);
+    this.$refs.map.myMap?.geoObjects.add(placemarkCollection)
   },
 
   methods: {
     getDiffTimestamp(locTimestamp, tempTimestamps) {
       return tempTimestamps.map((tempTimestamp) => {
-        return Math.abs(new Date(locTimestamp) - new Date(tempTimestamp));
-      });
+        return Math.abs(new Date(locTimestamp) - new Date(tempTimestamp))
+      })
     },
 
     getLocationTime(diffTimestamps, outsideTemps) {
-      const minTimestamp = Math.min(...diffTimestamps);
-      const indexMinTimestamp = diffTimestamps.indexOf(minTimestamp);
-      return outsideTemps[indexMinTimestamp];
+      const minTimestamp = Math.min(...diffTimestamps)
+      const indexMinTimestamp = diffTimestamps.indexOf(minTimestamp)
+      return outsideTemps[indexMinTimestamp]
     }
   }
-};
+}
 </script>
 
 <style>
